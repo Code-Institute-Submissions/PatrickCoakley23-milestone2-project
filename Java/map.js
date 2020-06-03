9b28ceb30a10981c85f94e38f6fd6a938a203576
PatrickCoakley23 <patrickcoakley23@gmail.com>
2020-06-03 17:02:27 +0000

    Added google places to map. Still very basic, but foundations set

--- Java/map.js
+++ Java/map.js
@@ -1,10 +1,59 @@
-       function initMap() {
+var map;
+var service;
+var infowindow;
+
+function initMap() {
+  var cork = new google.maps.LatLng(51.903614, -8.468399);
+
+  infowindow = new google.maps.InfoWindow();
+
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: cork,
+    zoom: 15,
+  });
+
+  var request = {
+    location: cork,
+    radius: "500",
+    query: "restaurant",
+  };
+
+  service = new google.maps.places.PlacesService(map);
+
+  service.findPlaceFromQuery(request, function (results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      for (var i = 0; i < results.length; i++) {
+        createMarker(results[i]);
+      }
+
+      map.setCenter(results[0].geometry.location);
+    }
+  });
+}
+
+function createMarker(place) {
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location,
+  });
+
+  google.maps.event.addListener(marker, "click", function () {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
+}
+
+/* function initMap() {
 
         var map = new google.maps.Map(document.getElementById("map"), {
           zoom: 10,
-          center: {lat: 51.903614, lng:  -8.468399}
+          center: {lat: 51.903614, -8.468399}
           });
+        }
 
+        */
+
+/*
           var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
            var markers = locations.map(function(location, i) {
@@ -21,3 +70,5 @@
         {lat: 51.838163314, lng: -8.487998048},
         {lat: 51.893497, lng:-8.491873},
     ]
+
+    */

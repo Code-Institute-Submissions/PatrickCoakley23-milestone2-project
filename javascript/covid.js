0e353c505662733d30f8a461609f128755635817
PatrickCoakley23 <patrickcoakley23@gmail.com>
2020-06-12 10:24:49 +0000

    Added guideline's section and travel advice section to covid container and also uploaded covid 19 poster

--- javascript/covid.js
+++ javascript/covid.js
@@ -5,7 +5,6 @@ var settings = {
   "timeout": 0,
 };
 
-
 $.ajax(settings).done(function (response) {
   var country = response.Countries[80].Country;
   var cases = response.Countries[80].NewConfirmed;
@@ -13,12 +12,14 @@ $.ajax(settings).done(function (response) {
   var totalDeaths = response.Countries[80].TotalDeaths;
   var totalRecovered = response.Countries[80].TotalRecovered;
   var date = response.Countries[80].Date;
+  var totalCases = response.Countries[80].TotalConfirmed;
 
   $covidData.prepend(`<tr><td><strong>Country:</strong></td> <td>${country}</td></tr>
                      <tr><td><strong>New Confirmed Cases:</strong></td> <td>${cases}</td></tr>
-                     <tr><td><strong>New Deaths:</strong></td> <td>${newDeaths}</td></tr>
-                     <tr><td><strong>Total Deaths:</strong></td> <td>${totalDeaths}</td></tr>
+                     <tr><td><strong>Confirmed Cases:</strong></td> <td>${totalCases}</td></tr>
                      <tr><td><strong>Total Recovered:</strong></td> <td>${totalRecovered}</td></tr>
+                     <tr><td><strong>New Deaths:</strong></td> <td>${newDeaths}</td></tr>
+                     <tr><td><strong>Total Deaths:</strong></td> <td>${totalDeaths}</td></tr> 
                      <tr><td><strong>Last Updated (GMT+0):</strong></td> <td>${date}</td></tr>
                     `)
                     
